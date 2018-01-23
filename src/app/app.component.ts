4e610f5fa61357492910a6cdacef8a0d60f3ebb0
raycar5 <raycar5@hotmail.es>
2018-01-23 23:10:30 +0100

    finally fixed sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,20 @@ import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/map';
 import { toStream } from 'mobx-utils';
 import { GlobalUIStore } from './stores/global.store';
-import { observable, action, isObservable } from 'mobx';
+import { observable, action, isObservable, autorun } from 'mobx';
 
 import { MatButton } from '@angular/material';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  public sidebarOpen$: Observable<boolean>;
   constructor(public globalUI: GlobalUIStore) {
-    this.sidebarOpen$ = Observable.from(toStream(() => globalUI.test));
   }
 
   ngOnInit(): void {
